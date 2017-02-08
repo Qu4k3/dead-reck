393d5cdb1c890a57142ad84896798f66ca19f32d
paul.guillamon@gmail.com <darksector>
2017-02-08 12:23:36 +0100

    update 6

--- src/game.js
+++ src/game.js
@@ -4,7 +4,7 @@ const randomColor = require('randomcolor')
 const deepEqual = require('deep-equal')
 
 document.addEventListener('keydown', function (event) {
-    event.preventDefault()
+    // event.preventDefault()
 })
 
 const socket = io()
@@ -26,24 +26,34 @@ let myPlayerId = null
 // hash playerId => playerData
 let players = {}
 
-function logic () {
-    // JSON for two equal objects should be the same string
-    // const oldInputs = JSON.stringify(Object.assign({}, myPlayer.inputs))
+const ACCEL = 1 / 500
 
+function updateInputs () {
     const { inputs } = myPlayer
-    const oldInputs = Object.assign({},  inputs)
 
     for (let key in inputs) {
         inputs[key] = kbd.isKeyDown(kbd[key])
     }
+}
 
-    if (inputs.LEFT_ARROW) myPlayer.vx--
-    if (inputs.RIGHT_ARROW) myPlayer.vx++
-    if (inputs.UP_ARROW) myPlayer.vy--
-    if (inputs.DOWN_ARROW) myPlayer.vy++
+function logic (delta) {
+    // JSON for two equal objects should be the same string
+    // const oldInputs = JSON.stringify(Object.assign({}, myPlayer.inputs))
+    const oldInputs = Object.assign({},  myPlayer.inputs)
+    updateInputs()
 
-    myPlayer.x += myPlayer.vx
-    myPlayer.y += myPlayer.vy
+    const vInc = ACCEL * delta
+    for (let playerId in players) {
+        const player = players[playerId]
+        const { inputs } = player
+        if (inputs.LEFT_ARROW) player.vx -= vInc
+        if (inputs.RIGHT_ARROW) player.vx += vInc
+        if (inputs.UP_ARROW) player.vy -= vInc
+        if (inputs.DOWN_ARROW) player.vy += vInc
+
+        player.x += player.vx * delta
+        player.y += player.vy * delta
+    }
 
     if (!deepEqual(myPlayer.inputs, oldInputs)) {
         socket.emit('move', myPlayer)
@@ -71,11 +81,25 @@ function render () {
     }
 }
 
+let past = Date.now()
 function gameloop () {
     requestAnimationFrame(gameloop)
-    logic()
+    const now = Date.now()
+    const delta = now - past
+    past = now
+    logic(delta)
     render()
 }
+
+let lastPingTimestamp
+let clockDiff = 0 // how many ms the server is ahead from us
+let ping = Infinity
+
+function startPingHandshake () {
+    lastPingTimestamp = Date.now()
+    socket.emit('game:ping')
+}
+setInterval(startPingHandshake, 250)
 
 socket.on('connect', function () {
     socket.on('world:init', function (serverPlayers, myId) {
@@ -87,10 +111,38 @@ socket.on('connect', function () {
 
     socket.on('playerMoved', function (player) {
         players[player.id] = player
+        const delta = (Date.now() + clockDiff) - player.timestamp
+
+        // increment position due to current velocity
+        // and update our velocity accordingly
+        player.x += player.vx * delta
+        player.y += player.vy * delta
+
+        const { inputs } = player
+        if (inputs.LEFT_ARROW && !inputs.RIGHT_ARROW) {
+            player.x -= ACCEL * Math.pow(delta, 2) / 2
+            player.vx -= ACCEL * delta
+        } else if (!inputs.LEFT_ARROW && inputs.RIGHT_ARROW) {
+            player.x += ACCEL * Math.pow(delta, 2) / 2
+            player.vx += ACCEL * delta
+        }
+        if (inputs.UP_ARROW && !inputs.DOWN_ARROW) {
+            player.y -= ACCEL * Math.pow(delta, 2) / 2
+            player.vy -= ACCEL * delta
+        } else if (!inputs.UP_ARROW && inputs.DOWN_ARROW) {
+            player.y += ACCEL * Math.pow(delta, 2) / 2
+            player.vy += ACCEL * delta
+        }
     })
 
     socket.on('playerDisconnected', function (playerId) {
         delete players[playerId]
+    })
+
+    socket.on('game:pong', (serverNow) => {
+        ping = (Date.now() - lastPingTimestamp) / 2
+        clockDiff = (serverNow + ping) - Date.now()
+        console.log({ ping, clockDiff })
     })
 })
 
